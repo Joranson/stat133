7d4e09a49851d2a303054f72b249303edb52c9fa
Qiyin Wu <qiyin.wu.jor@gmail.com>
2015-03-20 12:01:52 -0700

    hw6 done

--- hw6/hw6.r
+++ hw6/hw6.r
@@ -27,18 +27,18 @@
 #                 the entries are 0 for days where the doctor is a
 #                 non-adopter, else 1 (so once a row turns to 1 it stays as 1).
 
-sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
-
-  # Set up the output variable, define it as a matrix then use initial.doctors
-  # to set the first column (day)
-
-  # Run a simulation for <n.days> (use a for loop).  In the loop:
-  # 1) pick two random doctors
-  # 2) check if one has adopted the other hasn't
-  # 3) convert the non-adopter with probability p
-
-  # return the output
-
+sim.doctors <- function(initial.doctors, n.doctors, n.days, p){  
+  has_adopted <- matrix(data=initial.doctors,nrow=n.doctors,ncol=n.days)
+  for (j in 2:ncol(has_adopted)) {
+    row.position <- sample(c(1:n.doctors),size=2)
+    my.doctors <- has_adopted[row.position,j-1]
+    if (my.doctors[1]==1 | my.doctors[2]==1) {
+      if (runif(1)<p) {
+        has_adopted[row.position,j:ncol(has_adopted)] <- 1
+      }
+    }
+  }
+  return(has_adopted)
 }
 
 # When you test your function you have to generate <initial.doctors> and
@@ -51,3 +51,21 @@ set.seed(42)
 # on y-axis : the number of doctors that have already adopted the drug, on that day
 # Put all 5 lines in one figure (e.g. use first plot() then lines() for the subsequent lines)
 
+p <- c(0.1,0.3,0.5,0.7,0.9)
+newMatrix <- matrix(ncol=5,nrow=1000)
+for (i in 1:5) {
+  adopted <- sim.doctors(initial.doctors=sample(c(0,1),size=100,replace=T,prob=c(.9,.1)),n.doctors=100,n.days=1000,p[i])
+  line <- colSums(adopted)
+  newMatrix[,i] <- line
+}
+
+plot(newMatrix[,1],type="l",col="green",xlab="days",ylab="doctors adopting drug",main="Number of Doctors Adopting Drugs by Day",ylim=c(0,100))
+lines(newMatrix[,2],col="blue")
+lines(newMatrix[,3],col="red")
+lines(newMatrix[,4],col="orange")
+lines(newMatrix[,5],col="purple")
+text(x=800,y=45,"p=0.1", cex=0.7)
+text(x=600,y=80,"p=0.3", cex=0.7)
+text(x=400,y=80,"p=0.5", cex=0.7)
+text(x=300,y=70,"p=0.7", cex=0.7)
+text(x=200,y=80,"p=0.9", cex=0.7)
