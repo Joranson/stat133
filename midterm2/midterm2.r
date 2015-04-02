fbd66356592ed7a05e2bfb8b96a75a402eec19dd
Qiyin Wu <qiyin.wu.jor@gmail.com>
2015-04-02 15:30:05 -0700

    done midterm2

--- midterm2/midterm2.r
+++ midterm2/midterm2.r
@@ -7,20 +7,20 @@
 # Create [x], a numeric vector of length 1000 with 
 # entries: 6, 12, 18, etc.
 
-#x <- <your code here>
+x <- 6*c(1:1000)
 
 
 # [1 pt]
 # Create [y], a logical vector of length 2000 
 # with y[i]=T if x[i] is divisible by 10, otherwise F
 
-# y <- <your code here>
+y <- x%%10==0
 
 # [1 pt]
 # Create [w], a random permutation of the numeric values of a deck of cards
 # (i.e. just the numbers 1 through 13 each repeated 4 times)
 set.seed(2718)
-#w <- <your code here>
+w <- rep(sample(1:13, 13, replace=F), each=4)
 
 
 # [1 pt]
@@ -29,14 +29,14 @@ set.seed(2718)
 # (arrange the values by column, as per default)
 set.seed(344)
 
-#m <- <your code here>
+m <- matrix(rexp(100,3), nrow=10)
 
 
 # [1 pt]
 # Create [l], a list with 12 elements, each a vector of length 100.
 # Each vector of length 100 of Poisson (hint:rpois) random variables with mean 5
 set.seed(71)
-#l <- <your code here>
+l <- list(rpois(100,5), rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5),rpois(100,5))
 
 
 # for the next two tasks you will use the data frame infants (size 1236x15)
@@ -45,12 +45,12 @@ load("KaiserBabies.rda")
 
 # [2 pt]
 # Create a table [t] of the education level ($ed) of all married ($marital) first time ($parity=1) mothers:
-#t <- <your code here>
+t <- table(subset(infants, (marital=="Married") & (parity==1))$ed)
 
 
 # [2 pt]
 # Calculate [mw], the average birthweight ($bwt) of all babies whose were full term, i.e. gestation equal or more than 259 days.
-#mw <- <your code here>
+mw <- mean(subset(infants, gestation>=259)$bwt)
 
 
 # For the next few tasks you will use the data frame family (size 14x5)
@@ -59,23 +59,23 @@ load("family.rda")
 
 # [1 pt]
 # Create [f1] a subset of family with only women over age 50
-#f <- <your code here>
+f <- subset(family, (gender=="f") & (age>50))
 
   
 # [1 pt]
 # Create [f2] a subset of family with only men 6 foot tall or more
-#fm <- <your code here>
+fm <- subset(family, (gender=="m") & (height>=72))
 
   
 # [1 pt]
 # Create [f3] a subset of family of people whose name starts with T
-#f3 <- <your code here>
+f3 <- family[grep("T.", family$name),]
   
 
 
 # [1 pt]
 # Create [f4] a subset of family with just the youngest individual (so just one row)
-#f4 <- <your code here>
+f4 <- family[which(family$age==min(family$age)),]
 
 
 
@@ -89,20 +89,20 @@ load("family.rda")
 
 # [2 pts]
 # Make a box plot of Sepal Length by Species (so 3 boxplots in one plot)
-
-
+boxplot(iris[1:50,1], iris[51:100,1], iris[101:150,1])
 
 # [3 pts]
 # Make a scatterplot of petal width (y-axis) versus petal length (x-axis)
 # The axes labels should be "Petal Length" and "Petal Width",
 # Color the plotting symbol by Species (any 3 colors)
+plot(iris$Petal.Length, iris$Petal.Width, col =iris$Species)
 
 
 
 # [3 pt]
 # Make a scatterplot of ( sepal length / petal length) as a function of index (order)
 # Color the plotting symbol by Species (any 3 colors)
-
+plot(iris$Sepal.Length/iris$Petal.Length, 1:150, col=iris$Species)
 
 ##  apply statements
 
@@ -114,22 +114,21 @@ load("Cache500.rda")
 # [3 pts]
 # Create [first.cache], a vector where each entry is the _first_ element of the
 # corresponding vector in the list Cache500
-
-#first.cache <- <your code here>
+first.cache <- rapply(Cache500, function(x) head(x[1], 1))
 
 
 # [3 pts]
 # Create [mean.cache], a vector of length 500 where each entry is the mean 
 # of the corresponding element of the list Cache500
 
-#mean.cache <- <your code here>
+mean.cache <- rapply(Cache500, mean)
 
 
 # [2 pts]
 # Create [sd.cache], a vector of length 500 where each entry is the sd
 # of the corresponding element of the list Cache500
 
-#sd.cache <- <your code here>
+sd.cache <- rapply(Cache500, sd)
   
 
 
@@ -138,8 +137,14 @@ load("Cache500.rda")
 # mean.long.cache[i] is:
 # the mean of Cache500[[i]] IF it has 50 or more entries.
 # NA IF Cache500[[i]] has less than 50 entries.
-
-#mean.long.cache <- <your code here>
+func <- function(x) {
+  if (length(x)>=50) {
+    return (mean(x))
+  } else {
+    return (NA)
+  }
+}
+mean.long.cache <- rapply(Cache500, func)
 
 
 
